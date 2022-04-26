1ed085ff9d3550f2ad86f26a2188ae4bd0eb71e1
Ярослав Краснопёров <y.krasnoperov@seuslab.ru>
2022-04-26 20:43:35 +0500

    dev branch, front and back subprojects

---
+++ front/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: ["quasar"],
+
+  lintOnSave: false,
+
+  pluginOptions: {
+    quasar: {
+      importStrategy: "kebab",
+      rtlSupport: false,
+    },
+  },
+});
