9dddc57c2c3cca66b240b309f61f92a59d5bf282
Ярослав Краснопёров <y.krasnoperov@seuslab.ru>
2022-05-10 12:44:03 +0500

    user post splited on lines

--- front/src/components/UserFeedBlock.vue
+++ front/src/components/UserFeedBlock.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <q-card style="margin-bottom: 1rem;">
+    <q-card style="margin-bottom: 1rem">
       <q-card-section>
         <q-item-label class="text-h6 q-mb-xs text-bold">
           Записи пользователя
           <q-btn
-                  v-if="isYourPage"
-                  fab
-                  icon="add"
-                  color="primary"
-                  class="float-right"
+            v-if="isYourPage"
+            fab
+            icon="add"
+            color="primary"
+            class="float-right"
           />
         </q-item-label>
       </q-card-section>
@@ -17,16 +17,18 @@
       <q-separator />
     </q-card>
 
-    <q-card v-for="item in feed" style="margin-bottom: 1rem;">
+    <q-card v-for="item in feed" style="margin-bottom: 1rem">
       <q-card-section>
         <q-item-label class="text-h6 q-mb-xs">
-          {{item.title}}
+          {{ item.title }}
         </q-item-label>
-        <small>{{item.date}}</small>
+        <small>{{ item.date }}</small>
       </q-card-section>
 
       <q-card-section>
-        {{item.text}}
+        <p v-for="line in makeLines(item.text)">
+          {{ line }}
+        </p>
       </q-card-section>
     </q-card>
   </div>
@@ -43,9 +45,14 @@ export default {
     feed: {
       type: Array,
       default() {
-        return []
-      }
-    }
+        return [];
+      },
+    },
+  },
+  methods: {
+    makeLines(text) {
+      return text.split("\n");
+    },
   },
 };
 </script>
